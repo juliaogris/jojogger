7a2e8d4ddcf143d1c6cd92e1692b4d7462a03367
Julia Ogris <julia.ogris@gmail.com>
2017-03-31 11:37:15 +1100

    ⛑  Add Runner Favicon

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
+const emojiFavicon = require('emoji-favicon')
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+// Runner favicon
+app.use(emojiFavicon('runner'))
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')))
