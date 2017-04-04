ba5af100048e0e92de05d99a4b2fba0f30c450ec
Julia Ogris <julia.ogris@gmail.com>
2017-04-05 00:06:58 +1000

    ♻️   Rework Row component

--- react-ui/src/components/Row.js
+++ react-ui/src/components/Row.js
@@ -1,8 +1,42 @@
 import React, { Component, PropTypes } from 'react'
+import moment from 'moment'
 import Pencil from '../svgs/Pencil'
-import { getSpeed } from '../util/funcs'
 
-function trimDuration (duration) {
+export default class Row extends Component {
+  render () {
+    const { onEdit, id, jog } = this.props
+    const { date } = jog
+    const distance = formatDistance(jog.distance)
+    const duration = formatDuration(jog.duration)
+    const speed = getSpeed(duration, distance)
+
+    return (
+      <tr>
+        <td>{date}</td>
+        <td>{distance}</td>
+        <td>{duration}</td>
+        <td>{speed}</td>
+        { onEdit && <td> <EditButton onEdit={onEdit} id={id} /> </td> }
+      </tr>
+    )
+  }
+}
+
+Row.propTypes = {
+  jog: PropTypes.object.isRequired
+}
+
+function formatDistance (distance) {
+  if (distance === 0) {
+    return '-'
+  }
+  return distance.toFixed(2)
+}
+
+function formatDuration (duration) {
+  if (duration === '00:00:00' || duration === '0:00:00') {
+    return '-'
+  }
   let d = duration
   if (d.startsWith('00:')) {
     d = d.substring(3)
@@ -13,33 +47,18 @@ function trimDuration (duration) {
   return d
 }
 
+function getSpeed (timeStr, distanceKm) {
+  const hours = moment.duration(timeStr).asHours()
+  if (hours === 0) {
+    return '-'
+  }
+  return (distanceKm / hours).toFixed(2)
+}
+
 function EditButton ({ onEdit, id }) {
   return (
     <button onClick={() => onEdit(id)} className='edit-button'>
       <Pencil color={'#777'} />
     </button>
   )
-}
-
-export default class Row extends Component {
-  render () {
-    const { onEdit, id, jog } = this.props
-    const { date, duration, distance } = jog
-    const trimmedDuration = trimDuration(duration)
-    const speed = getSpeed(duration, distance).toFixed(2)
-
-    return (
-      <tr>
-        <td>{date}</td>
-        <td>{distance.toFixed(2)}</td>
-        <td>{trimmedDuration}</td>
-        <td>{speed}</td>
-        { onEdit && <td> <EditButton onEdit={onEdit} id={id} /> </td> }
-      </tr>
-    )
-  }
-}
-
-Row.propTypes = {
-  jog: PropTypes.object.isRequired
 }
