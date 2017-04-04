4dfe60424bf51216c4aabb290db7d2bf9e6e00a1
Julia Ogris <julia.ogris@gmail.com>
2017-04-04 21:47:36 +1000

    🌱  Initialize Rest API fetch functions

---
+++ react-ui/src/util/api.js
@@ -0,0 +1,37 @@
+/* global fetch btoa */
+
+function handleErrors (response) {
+  if (response.status === 401) {
+    throw Error('Invalid username or password.')
+  }
+  if (!response.ok) {
+    console.err('Internal Error', response.status, response.statusText)
+    throw Error('We are sorry, something went wrong.')
+  }
+  return response
+}
+
+export function apiLogin (email, password) {
+  return fetch('/api/login', {
+    headers: {
+      Authorization: 'Basic ' + btoa(`${email}:${password}`)
+    }
+  })
+  .then(handleErrors)
+  .then(response => response.json())
+  .then(user => {
+    user.password = password
+    return user
+  })
+}
+
+export function apiGetJogs (user) {
+  const { email, password, id } = user
+  return fetch(`/api/users/${id}/jogs`, {
+    headers: {
+      Authorization: 'Basic ' + btoa(`${email}:${password}`)
+    }
+  })
+  .then(handleErrors)
+  .then(response => response.json())
+}
