d70d9f94b9d8e58b7d6ce89e1e8c54a28f1e4d06
Julia Ogris <julia.ogris@gmail.com>
2017-04-09 15:10:51 +1000

    SSL on heroku

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
 
 // const express = require('express')
 // const path = require('path')
